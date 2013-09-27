f9c5c68a021dc9f00349d5ed6bbfdec5cc48a5d2
fschwiet <fschwiet@gmail.com>
2013-09-27 12:10:50 -0700

    generating a filename

--- jakefile.js
+++ jakefile.js
@@ -1,5 +1,6 @@
 
 var Q = require("q");
+var sprintf = require("sprintf").sprintf;
 var childProcess = require("child_process");
 
 var mysqlDumpPath = "C:/mysql/bin/mysqldump.exe";
@@ -12,7 +13,19 @@ var outputFile = "./test.sql";
 
 task("createBackup", function() {
 
-    return Q.nfcall(childProcess.spawn, mysqlDumpPath, ["--host",  hostname, "-u", username, "-p", password, "-r", outputFile, databaseName], {
+    var now = new Date();
+
+    var filename = sprintf("%s.%04d-%02d-%02d.%02d-%02d-%02d.sql", databaseName, now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds());
+
+    console.log("Generating backup", filename);
+
+    var mysqldumpArgs = ["--host",  hostname, "-u", username, "-r", outputFile, databaseName];
+
+    if (password !== null) {
+        mysqldumpArgs.splice(0,0, "--password=" + password);
+    }
+
+    return Q.nfcall(childProcess.spawn, mysqlDumpPath, mysqldumpArgs, {
         stdio: "inherit"
     })
     .then(function(results) {
